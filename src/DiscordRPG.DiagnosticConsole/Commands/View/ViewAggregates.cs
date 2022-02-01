3dfe29ea5e9aca4e8a82b6a8f34377ccc526ef4b
Joshua.Hillmann@adesso.de <Joshua.Hillmann@adesso.de>
2022-02-01 13:28:59 +0100

    aggregate viewer

---
+++ src/DiscordRPG.DiagnosticConsole/Commands/View/ViewAggregates.cs
@@ -0,0 +1,43 @@
+﻿using DiscordRPG.DiagnosticConsole.Importers;
+using DiscordRPG.DiagnosticConsole.Writers;
+using DiscordRPG.Domain.Aggregates.Guild;
+using Spectre.Console;
+
+namespace DiscordRPG.DiagnosticConsole.Commands.View;
+
+public class ViewAggregates : ICommand
+{
+    private readonly AggregateImporter aggregateImporter;
+
+    public ViewAggregates(AggregateImporter aggregateImporter)
+    {
+        this.aggregateImporter = aggregateImporter;
+    }
+
+    public static string Command => "aggregates";
+    public string CommandName => Command;
+
+    public async Task ExecuteAsync(IEnumerable<ICommand> commands)
+    {
+        AnsiConsole.Clear();
+        string result = "";
+        while (result != "back")
+        {
+            IEnumerable<string> ids = null;
+            await AnsiConsole.Status().StartAsync("Loading aggregates...",
+                async context => { ids = await aggregateImporter.LoadAllAggregateIds(); });
+            result = AnsiConsole.Prompt<string>(new SelectionPrompt<string>().AddChoices(ids.Append("back")));
+            if (result == "back")
+                continue;
+
+            GuildAggregate aggregate = null;
+            await AnsiConsole.Status().StartAsync($"Loading {result}...",
+                async context =>
+                {
+                    aggregate = await aggregateImporter.LoadAggregate<GuildAggregate, GuildId>(new GuildId(result));
+                });
+
+            AggregateWriter.Write(aggregate);
+        }
+    }
+}
---
+++ src/DiscordRPG.DiagnosticConsole/Writers/AggregateWriter.cs
@@ -0,0 +1,37 @@
+﻿using DiscordRPG.Domain.Aggregates.Guild;
+using Spectre.Console;
+
+namespace DiscordRPG.DiagnosticConsole.Writers;
+
+public class AggregateWriter
+{
+    public static void Write(GuildAggregate aggregate)
+    {
+        var table = new Table();
+        table.AddColumn("Name");
+        table.AddColumn("Characters");
+        table.AddColumn("Dungeons");
+
+        var charTable = new Table();
+        charTable.AddColumn("Name");
+        charTable.AddColumn("Class");
+        charTable.AddColumn("Level");
+        foreach (var character in aggregate.Characters)
+        {
+            charTable.AddRow(character.Name.Value, $"{character.Race.Name} {character.Class.Name}",
+                character.CharacterLevel.CurrentLevel.ToString());
+        }
+
+        var dungeonTable = new Table();
+        dungeonTable.AddColumn("Name");
+        dungeonTable.AddColumn("Rarity");
+        dungeonTable.AddColumn("Level");
+        foreach (var dungeon in aggregate.Dungeons)
+        {
+            dungeonTable.AddRow(dungeon.Name.Value, dungeon.Rarity.ToString(), dungeon.Level.ToString());
+        }
+
+        table.AddRow(new Panel(aggregate.GuildName), charTable, dungeonTable);
+        AnsiConsole.Write(table);
+    }
+}
